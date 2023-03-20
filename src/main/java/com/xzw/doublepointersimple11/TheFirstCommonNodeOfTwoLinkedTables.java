f8c759a88a5c07019d42d24e35989549de1ccba9
maroon <1582891731@qq.com>
2023-03-20 17:52:43 +0800

    add 第12天：双指针（简单）

---
+++ src/main/java/com/xzw/doublepointersimple11/TheFirstCommonNodeOfTwoLinkedTables.java
@@ -0,0 +1,19 @@
+package com.xzw.doublepointersimple11;
+
+/**
+ * @author maroon
+ * @date 2023/3/20 17:49
+ * 输入两个链表，找出它们的第一个公共节点。
+ */
+public class TheFirstCommonNodeOfTwoLinkedTables {
+}
+class Solution4 {
+    ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode n1 = headA, n2 = headB;
+        while (n1 != n2) {
+            n1 = n1 != null ? n1.next : headB;
+            n2 = n2 != null ? n2.next : headA;
+        }
+        return n1;
+    }
+}

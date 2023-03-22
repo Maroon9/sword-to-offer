eccf64213a94b86ff54f170a5d3cbac547365894
maroon <1582891731@qq.com>
2023-03-22 17:20:41 +0800

    add 第14天：搜索与回溯算法（中等）

---
+++ src/main/java/com/xzw/searchandbacktrackingalgorithmmedium14/RangeOfMotionOfTheRobot.java
@@ -0,0 +1,32 @@
+package com.xzw.searchandbacktrackingalgorithmmedium14;
+
+/**
+ * @author maroon
+ * @date 2023/3/22 15:56
+ * 地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。一个机器人从坐标 [0, 0] 的格子开始移动，
+ * 它每次可以向左、右、上、下移动一格（不能移动到方格外），也不能进入行坐标和列坐标的数位之和大于k的格子。
+ * 例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。但它不能进入方格 [35, 38]，因为3+5+3+8=19。
+ * 请问该机器人能够到达多少个格子？
+ *
+ */
+public class RangeOfMotionOfTheRobot {
+    public static void main(String[] args) {
+        System.out.println(Solution2.movingCount(2, 3, 1));
+    }
+}
+class Solution2 {
+    public static int movingCount(int m, int n, int k) {
+        boolean[][] visited = new boolean[m][n];
+        return dfs(0, 0, m, n, k, visited);
+    }
+
+    private static int dfs(int i, int j, int m, int n, int k, boolean[][] visited) {
+        if (i < 0 || i >= m || j < 0 || j >= n || (i/10 + i%10 + j/10 + j%10) > k || visited[i][j]) {
+            return 0;
+        }
+        visited[i][j] = true;
+        int res = dfs(i + 1, j, m, n, k, visited) + dfs(i - 1, j, m, n, k, visited) +
+                dfs(i, j + 1, m, n, k, visited) + dfs(i, j - 1, m, n, k, visited) + 1;
+        return res;
+    }
+}
